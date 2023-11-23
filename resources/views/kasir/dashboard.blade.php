d21d5797504b7067afe557172145cb2555a7d683
AchmadRizqullahBlessar <essar2002@gmail.com>
2023-11-24 01:00:06 +0700

    first commit

---
+++ resources/views/kasir/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="active">Dashboard</li>
+@endsection
+
+@section('content')
+<!-- Small boxes (Stat box) -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="box">
+            <div class="box-body text-center">
+                <h1>Selamat Datang</h1>
+                <h1>Anda login sebagai Owner</h1>
+                <h3>Klik tombol dibawah untuk mengecek Laporan bulan ini</h3>
+                <br><br>
+                <a href="{{ route('laporan.index') }}" class="btn btn-success btn-lg">Laporan</a>
+                <br><br><br>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.row (main row) -->
+@endsection
